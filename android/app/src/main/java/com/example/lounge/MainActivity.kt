740e3cb80b86a34a8f8f6a549edec47bd3b0e940
Sarahparah <90765320+Sarahparah@users.noreply.github.com>
2022-03-30 09:13:36 +0200

    android files

---
+++ android/app/src/main/java/com/example/lounge/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lounge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //klsdcsd
+}
